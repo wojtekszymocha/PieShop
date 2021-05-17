88ecb659cff6ac4b9c37b80b5265fd2b54e25173
szymocha.wojtek@outlook.com <szymocha.wojtek@outlook.com>
2021-05-17 13:36:31 +0200

    Add Controller, Views and ViewModel

---
+++ PieShop/Controllers/PieController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PieShop.Models;
+using PieShop.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PieShop.Controllers
+{
+    public class PieController : Controller
+    {
+        private readonly IPieRepository pieRepository;
+        private readonly ICategoryRepository categoryRepository;
+
+        public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+        {
+            this.pieRepository = pieRepository;
+            this.categoryRepository = categoryRepository;
+        }
+
+        public ViewResult List()
+        {
+            PieListViewModel pieListViewModel = new PieListViewModel();
+            pieListViewModel.Pies = pieRepository.Pies;
+            pieListViewModel.CurrentCateogry = "Cheese Cake";
+            return View(pieListViewModel);
+        }
+    }
+}
